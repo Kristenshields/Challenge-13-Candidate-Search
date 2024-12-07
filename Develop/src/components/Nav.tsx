1186dce73535cedb3a17f1badebc6f7babeb8bb1
Kristen Shields <shieldskristen0@gmail.com>
2024-12-06 18:18:36 -0700

    more debugging

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -5,13 +5,16 @@ const Nav = () => {
     return (
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li>
+            <Link to="/">Search</Link>
+          </li>
+          <li>
+            <Link to="/SavedCandidates">Saved Candidates</Link>
+          </li>
         </ul>
       </nav>
     );
-};
+  };
 
 export default Nav;
   
