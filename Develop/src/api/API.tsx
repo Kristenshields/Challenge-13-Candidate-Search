27cf559fdff1bed34508826b8888fa39b616a8e4
Kristen Shields <shieldskristen0@gmail.com>
2024-12-06 18:00:43 -0700

    working on getting API to connect

--- Develop/src/api/API.tsx
+++ Develop/src/api/API.tsx
@@ -1,7 +1,10 @@
 const searchGithub = async () => {
     try {
       const start = Math.floor(Math.random() * 100000000) + 1;
-      console.log(import.meta.env);
+      console.log('Environment Variables:', import.meta.env);
+      if (!import.meta.env.VITE_GITHUB_TOKEN) {
+        throw new Error('GitHub token is not set in environment variables');
+      }
       const response = await fetch(
         `https://api.github.com/users?since=${start}`,
         {
@@ -11,10 +14,10 @@ const searchGithub = async () => {
         }
       );
        console.log('Response:', response);
-      const data = await response.json();
       if (!response.ok) {
         throw new Error('invalid API response, check the network tab');
       }
+      const data = await response.json();
        console.log('Data:', data);
       return data;
     } catch (err) {
